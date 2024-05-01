bac3c3f4d44216644f9644cb80158d9413a96cd1
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-05-01 22:00:25 +0200

    [Terminer] Jour1/Job00

---
+++ Jour01/Job00/Job00.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+    cout << "a = x + 5" << endl;
+
+    cout << "a = x = y + 2" << endl;
+
+    cout << "a = x == y" << endl;
+
+    cout << "(a < b) && (c < d)" << endl;
+
+    cout << "(i++) * (n + p)"<< endl;
+
+    return 0;
+}
