680e01904681dbd6a627f68ef7ad5a531e016863
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-05-02 10:39:54 +0200

    [Terminer] Jour03/Job04

---
+++ Jour03/Job04/Job04.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(void) {
+    char arr[] = "Vive la plateforme !";
+
+    int len = strlen(arr);
+
+    cout << arr << endl;
+    cout << "La taille est " << len << endl;
+
+    return 0;
+}
