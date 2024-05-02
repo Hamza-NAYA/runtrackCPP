d281739078577e677016d6e9dc01decf6039c844
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-05-02 10:57:04 +0200

    [Terminer] Jour03/Job06

---
+++ Jour03/Job06/Job06.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int size = 10;
+    int T[size];
+    int count = 0;
+
+    cout << "Entrez 10 entiers :\n";
+    for (int i = 0; i < size; ++i) {
+        cout << "Entier " << i + 1 << " : ";
+        cin >> T[i];
+    }
+
+    for (int i = 0; i < size; ++i) {
+        if (T[i] >= 5) {
+            count++;
+        }
+    }
+
+    cout << "Nombre d'entiers >= 5 : " << count << endl;
+
+    return 0;
+}
