30bb6214fe09fbfee3de4b0c04c8a81c03471588
Hamza-NAYA <hamza.naya@laplateforme.io>
2024-05-12 14:53:42 +0200

    [Terminer] Jour02/Job07

---
+++ Jour02/Job07/Job07.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b;
+
+    cout << "Entrez la borne inferieure a : ";
+    cin >> a;
+
+    cout << "Entrez la borne superieure b : ";
+    cin >> b;
+
+    if (a <= b) {
+
+        for (int i = a; i <= b; i++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "La borne inferieure doit etre inferieure ou egale a la borne superieure" << endl;
+    }
+
+    return 0;
+}
